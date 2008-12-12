38de6c2b3c5eb0e41f4feb4d534a1acfaafdfc5b
Michael Starzinger <michi@complang.tuwien.ac.at>
2008-12-12 17:10:16 +0100

    * src/native/vm/openjdk/jvm.c (JVM_IsNaN): Implemented. (transplanted from d4a244a77bd8678ff525e7a289a0d7a9f6be363a)

--- src/native/vm/openjdk/jvm.c
+++ src/native/vm/openjdk/jvm.c
@@ -43,6 +43,8 @@
 
 #include "vm/types.h"
 
+#include "fdlibm/fdlibm.h"
+
 #include "mm/memory.h"
 
 #include "native/jni.h"
@@ -3045,11 +3047,17 @@ void *JVM_FindLibraryEntry(void *handle, const char *name)
 
 /* JVM_IsNaN */
 
-jboolean JVM_IsNaN(jdouble a)
+jboolean JVM_IsNaN(jdouble d)
 {
-	log_println("JVM_IsNaN: IMPLEMENT ME!");
+	bool result;
 
-	return 0;
+	TRACEJVMCALLSENTER(("JVM_IsNaN(d=%f)", d));
+
+	result = isnan(d);
+
+	TRACEJVMCALLSEXIT(("->%d", result));
+
+	return result;
 }
 
 
